2847c1c8cf12d8a12ad91b8e83348137e11aa380
bhabgs <bhabgs@aliyun.com>
2025-10-13 11:38:15 +0800

    refactor(router): 将 createBrowserRouter 替换为 createHashRouter
    为了支持静态文件服务器部署，改用哈希路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { routes } from './routes';
+import React from "react";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./routes";
 
 // 创建路由器
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 // 路由提供者组件
 const AppRouter: React.FC = () => {
